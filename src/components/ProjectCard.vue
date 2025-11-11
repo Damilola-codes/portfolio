b1a71063fe5662d709bfa4e6881d7497b1948993
Damilola Emmanuel <odeyemidamilola78@gmail.com>
2025-11-11 11:05:07 +0000

    updated my code

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -6,10 +6,12 @@
         class="w-full h-64 object-cover rounded-xl bg-gray-300 p-2"/>
         <h3 class="text-lg md:text-2xl mt-4 font-semibold text-gray-800">{{title}}</h3>
         <p class="text-sm text-gray-500 mt-1">{{ description}}</p>
-            <a href=""
-                target="_blank"
-                rel="noopener noreferrer"
-                class="mt-4 inline-flex items-center gap-2 text-sm font-medium transition group bg-gray-300 px-4 py-3 rounded-xl place-items-center">
+            <a
+                :href="link || '#'"
+                :target="link ? '_blank' : null"
+                :rel="link ? 'noopener noreferrer' : null"
+                class="mt-4 inline-flex items-center gap-2 text-sm font-medium transition group bg-gray-300 px-4 py-3 rounded-xl place-items-center"
+            >
                 View Project
                 <ChevronRightIcon class=" w-5 h-5  transition-transform duration-300 transform group-hover:translate-x-1"/>
             </a>
